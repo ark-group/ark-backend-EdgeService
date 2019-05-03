6c232a8c41bea060b205d7e7775780f31db94eb9
krrathore1990 <karanr15.rathore@gmail.com>
2019-05-04 01:37:57 +0530

    first commit

---
+++ src/main/java/com/ark/arkbackendEdgeService/ArkBackendEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ark.arkbackendEdgeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkBackendEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkBackendEdgeServiceApplication.class, args);
+	}
+
+}
